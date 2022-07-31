c4df1e1a74e6f7ccc882bad1eaba163c610ce457
Solo-steven <ssteven19@kimo.com>
2022-08-01 01:24:22 +0800

    Feat: Basic Virtual DOM

---
+++ demo/virtual-dom/src/render.ts
@@ -0,0 +1,28 @@
+import { VirtualNode, DOMNode } from "./type.d";
+
+export const createElement = (
+    type: string, props: {[key: string]: any}, children: Array<VirtualNode>
+) => {
+    return {
+        type, props, children
+    }
+};
+
+export const transform = (vdom: VirtualNode) => {
+    if(typeof vdom === 'string' ||typeof vdom === 'number') {
+        const node = document.createTextNode(String(vdom));
+        return node;
+    }
+    const node = document.createElement(vdom.type );
+    for(const [key, value] of Object.entries(vdom.props)) {
+            node.setAttribute(key, value)
+    }
+    for(const child of vdom.children) {
+        node.appendChild(transform(child));
+    }
+    return node;
+}
+
+export const mount = (dom: DOMNode, replacedDOM: Element) => {
+    replacedDOM.appendChild(dom);
+}
